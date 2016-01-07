f96b4919d33f319fbfc4db21ae46df639beeccd0
Luc Jean <ljean@apidev.fr>
2016-01-07 07:34:29 +0100

    use function rather that string in urls.py

--- djaloha/urls.py
+++ djaloha/urls.py
@@ -2,8 +2,8 @@
 """urls"""
 
 from django.conf.urls import patterns, url
-
+from djaloha.views import aloha_init
 
 urlpatterns = patterns('',
-    url(r'^djaloha/aloha-config.js', 'djaloha.views.aloha_init', name='aloha_init'),
+    url(r'^djaloha/aloha-config.js', aloha_init, name='aloha_init'),
 )
